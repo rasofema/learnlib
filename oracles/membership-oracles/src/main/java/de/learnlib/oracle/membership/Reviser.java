a4af2731211b06ce6c94419ade6a8e7fcf5d4570
Tiago Ferreira <me@tiferrei.com>
2022-09-14 14:00:23 -0400

    fix

--- oracles/membership-oracles/src/main/java/de/learnlib/oracle/membership/Reviser.java
+++ oracles/membership-oracles/src/main/java/de/learnlib/oracle/membership/Reviser.java
@@ -20,6 +20,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
