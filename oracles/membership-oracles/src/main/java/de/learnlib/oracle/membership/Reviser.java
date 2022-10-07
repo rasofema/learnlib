190c94917c6d4f2e7c18c660acb28898d777bc48
Tiago Ferreira <me@tiferrei.com>
2022-10-07 16:27:49 +0100

    refactor

--- oracles/membership-oracles/src/main/java/de/learnlib/oracle/membership/Reviser.java
+++ oracles/membership-oracles/src/main/java/de/learnlib/oracle/membership/Reviser.java
@@ -31,7 +31,6 @@ import de.learnlib.api.query.Query;
 import de.learnlib.filter.statistic.Counter;
 import net.automatalib.automata.transducers.MealyMachine;
 import net.automatalib.incremental.ConflictException;
-import net.automatalib.incremental.mealy.tree.Node;
 import net.automatalib.incremental.mealy.tree.AdaptiveMealyTreeBuilder;
 import net.automatalib.words.Alphabet;
 import net.automatalib.words.Word;
@@ -63,11 +62,8 @@ public class Reviser<S, I, T, O>
         Word<O> answer = sulOracle.answerQuery(query.getInput());
         query.answer(answer.suffix(query.getSuffix().length()));
 
-        Boolean conflicts = cache.conflicts(query.getInput(), answer);
-        cache.insert(query.getInput(), answer);
-
         // Conflict detected
-        if (conflicts) {
+        if (cache.insert(query.getInput(), answer)) {
             throw new ConflictException("Input: " + query.getInput());
         }
 
@@ -121,7 +117,7 @@ public class Reviser<S, I, T, O>
 
         while (counter.getCount() < limit) {
             DefaultQuery<I, Word<O>> query = new DefaultQuery<>(
-                    random.nextFloat() < revisionRatio ? (Word<I>) cache.getOldestQuery() : sampleWord());
+                    random.nextFloat() < revisionRatio ? (Word<I>) cache.getOldestInput() : sampleWord());
             Word<O> out = internalProcessQuery(query);
 
             if (!hypothesis.computeOutput(query.getInput()).equals(out)) {
